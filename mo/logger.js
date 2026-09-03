08c880ded6a1c1ec57a838e1fc9d049511a5f1f2
ashishreal58-pixel <ashishreal58@gmail.com>
2026-09-03 13:18:27 +0530

    Delete mo/logger.js

--- mo/logger.js
+++
@@ -1,9 +0,0 @@
-// modules/logger.js
-// Custom logging module with timestamps
-
-function log(message) {
-  const timestamp = new Date().toISOString();
-  console.log(`[${timestamp}] ${message}`);
-}
-
-module.exports = { log };
