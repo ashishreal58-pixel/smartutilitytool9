8c2b2a6e8381134d3f537d360373121ae5ebdf63
ashishreal58-pixel <ashishreal58@gmail.com>
2026-09-22 16:48:32 +0530

    Update dice.js

--- mo/dice.js
+++ mo/dice.js
@@ -2,6 +2,7 @@
 // Random dice generator using the crypto module for secure randomness
 
 const crypto = require('crypto');
+const logMessage = require('./modules/logger'); // Integrates your custom logger module
 
 function rollDice() {
   // crypto.randomInt(min, max) -> min inclusive, max exclusive
@@ -10,7 +11,7 @@ function rollDice() {
 
 const numberOfRolls = 5;
 
-console.log(`Rolling the dice ${numberOfRolls} times...\n`);
+logMessage(`Application triggered: Rolling the dice ${numberOfRolls} times...\n`);
 
 for (let i = 1; i <= numberOfRolls; i++) {
   console.log(`Roll ${i} -> Dice Rolled: ${rollDice()}`);
